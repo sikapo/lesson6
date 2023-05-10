e2ee2d2ee6f78df10a2d4a3992baa323023ae319
Owner <vitycity79@gmail.com>
2023-05-10 17:58:40 +0300

    Init commit

--- part_1/src/Main.java
+++ part_1/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        String[] array = {"Word", "wood", "Name", "Earth", "Car", "WOOD", "Wood", "Dog", "Girl", "space", "WorD", "YouTube", "wood wood", "Andrew", "woodwood"};
+        String word = "wood";
+        System.out.println(countOccurrence(array, word));
+    }
+    public  static int countOccurrence(String[] list, String word) {
+        int counter = 0;
+        for (String target: list) {
+            if (target.equalsIgnoreCase(word)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
