fea14f71046d6611a974e055804e3dd44bc1bcfa
Owner <vitycity79@gmail.com>
2023-05-10 18:13:19 +0300

    Init commit

--- part_1/src/Main.java
+++ part_1/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        //for countOccurrence method
         String[] array = {"Word", "wood", "Name", "Earth", "Car", "WOOD", "Wood", "Dog", "Girl", "space", "WorD", "YouTube", "wood wood", "Andrew", "woodwood"};
         String word = "wood";
-        System.out.println(countOccurrence(array, word));
+        //System.out.println(countOccurrence(array, word));
+
+        //for toList method
+        System.out.println(toList(array));
+
     }
     public  static int countOccurrence(String[] list, String word) {
         int counter = 0;
@@ -14,4 +23,12 @@ public class Main {
         }
         return counter;
     }
+
+    public static List<String> toList (String[] array) {
+
+        return new ArrayList<>(Arrays.asList(array));
+
+    }
+
+
 }
